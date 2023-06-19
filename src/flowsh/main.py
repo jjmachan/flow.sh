9e91a06a3204b69c9275c2f7726d46c1c1b99e03
jjmachan <jamesjithin97@gmail.com>
2023-06-19 15:50:38 +0530

    basic outline complete

--- src/flowsh/main.py
+++ src/flowsh/main.py
@@ -1,2 +1,47 @@
+from __future__ import annotations
+
+import typing as t
+
+from textual.app import App
+from textual.containers import Container, Horizontal
+from textual.widgets import Button, Footer, Header, Static
+
+if t.TYPE_CHECKING:
+    from textual.app import ComposeResult
+
+EMOJI_MAP = {
+    "unmute": "🔊",
+    "mute": "🔇",
+    "play": "▶️",
+    "pause": "⏸️",
+}
+
+
+class Timer(Static):
+    ...
+
+
+class Music(Static):
+    ...
+
+
+class TimerBar(Static):
+    def compose(self) -> ComposeResult:
+        yield Horizontal(Music("🔊  LofiGirl"), Timer("▶️  00:00:00"))
+
+
+class FlowshApp(App):
+    CSS_PATH = "./css/flowsh.css"
+    BINDINGS = [
+        ("q", "quit", "Quit"),
+        ("m", "mute", "Mute/Unmute the music"),
+        ("space", "pause", "Pause/Play the timer"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Container(TimerBar(), Footer())
+
+
 def main():
-    print("starting")
+    app = FlowshApp()
+    app.run()
