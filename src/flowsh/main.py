42d393f708981fbf1feacfa5a70a3e8e12029527
jjmachan <jamesjithin97@gmail.com>
2023-06-19 16:26:47 +0530

    finished mvp

--- src/flowsh/main.py
+++ src/flowsh/main.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import typing as t
+from time import monotonic
 
 from textual.app import App
 from textual.containers import Container, Horizontal
+from textual.reactive import reactive
 from textual.widgets import Button, Footer, Header, Static
 
 if t.TYPE_CHECKING:
@@ -18,11 +20,59 @@ EMOJI_MAP = {
 
 
 class Timer(Static):
-    ...
+    """
+    Displays elapsed time
+    """
+
+    is_paused = True
+    start_time = reactive(monotonic)
+    time = reactive(0.0)
+    total = reactive(0.0)
+    emoji = EMOJI_MAP["pause" if is_paused else "play"]
+
+    def on_mount(self) -> None:
+        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=True)
+
+    def update_time(self) -> None:
+        self.time = self.total + (monotonic() - self.start_time)
+
+    def watch_time(self, time: float) -> None:
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{self.emoji}  {hours:02.0f}:{minutes:02.0f}:{seconds:02.0f}")
+
+    def start(self) -> None:
+        self.emoji = EMOJI_MAP["play"]
+        self.start_time = monotonic()
+        self.update_timer.resume()
+
+    def stop(self) -> None:
+        self.emoji = EMOJI_MAP["pause"]
+        self.update_timer.pause()
+        self.total += monotonic() - self.start_time
+        self.time = self.total
+
+    def reset(self) -> None:
+        self.total = 0.0
+        self.time = 0.0
+
+    def toggle_pause(self) -> None:
+        if self.is_paused:
+            self.start()
+        else:
+            self.stop()
+
+        self.is_paused = not self.is_paused
 
 
 class Music(Static):
-    ...
+    muted: bool = False
+
+    def toggle_mute(self) -> None:
+        self.muted = not self.muted
+        self.update(
+            f"{EMOJI_MAP['mute'] if self.muted else EMOJI_MAP['unmute']}  LofiGirl"
+        )
 
 
 class TimerBar(Static):
@@ -37,6 +87,16 @@ class FlowshApp(App):
         ("m", "mute", "Mute/Unmute the music"),
         ("space", "pause", "Pause/Play the timer"),
     ]
+
+    def action_mute(self) -> None:
+        music = self.query_one("Music")
+        if music:
+            music.toggle_mute()
+
+    def action_pause(self) -> None:
+        timer = self.query_one("Timer")
+        if timer:
+            timer.toggle_pause()
 
     def compose(self) -> ComposeResult:
         yield Container(TimerBar(), Footer())
